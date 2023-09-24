5ab1d63ee1fb48f67128ebaa741bed298ffe3af9
Sergey <sergey.cosilov@gmail.com>
2023-09-24 19:43:50 +0300

    init add first block gsap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlockFirstComponent } from './block-first/block-first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockFirstComponent
   ],
   imports: [
     BrowserModule
